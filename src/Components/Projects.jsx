273cbc1fb6c6be52c0acb130bc67b169c5908de7
Tarun <tarunkumar23j@gmail.com>
2024-09-10 12:08:48 +0530

    renamed project name

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -40,7 +40,7 @@ const Projects = ({bgcolor,darkmode}) => {
               />
               <div>
 
-                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>My Portfolio Website (Reactjs,Tailwind,Javascript)</h1>
+                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>Portfolio Website (Reactjs,Tailwind,Javascript)</h1>
               </div>
             </div>
             </a>
