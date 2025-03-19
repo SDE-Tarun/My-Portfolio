da3001ac6af03d955d42b46be59dff7ed6b4aca1
Tarun <tarunkumar23j@gmail.com>
2025-03-19 11:45:00 +0530

    AI code reviewer project added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "../Components/Global.css";
-import project1 from "../Components/SocialLinks/e commerce ss.png"
-import project2 from "../Components/SocialLinks/My-Portfolio.png"
+import project1 from "../Components/SocialLinks/AI Code Reviewer.png"
+import project2 from "../Components/SocialLinks/Ecommerce.png"
+import project3 from "../Components/SocialLinks/Portfolio.png"
+
 
 
 
@@ -13,7 +15,8 @@ const Projects = ({bgcolor,darkmode}) => {
           <h1 className={`text-3xl font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>Projects</h1>
 
           <div className="flex gap-5 flex-wrap my-5">
-          <a href="https://amazing-toffee-71729d.netlify.app/" target="_blank">
+
+          <a href="https://code-reviewer-frontend-jqbz.onrender.com/" target="_blank">
             <div
               className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
               style={{background:`${bgcolor==="white" ? "#FFF4F4" :"black"}`,  border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}
@@ -24,12 +27,13 @@ const Projects = ({bgcolor,darkmode}) => {
               />
               <div>
 
-                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>E-commerce Clothing Website (Reactjs,Tailwind,Firebase)</h1>
+                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>AI Code Reviewer (MERN Stack + Google Gemini API)</h1>
               </div>
             </div>
             </a>
+          
 
-            <a href="https://lighthearted-zuccutto-7672ad.netlify.app/" target="_blank">
+          <a href="https://ecommerce-platform-rrxs.onrender.com/" target="_blank">
             <div
               className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
               style={{background:`${bgcolor==="white" ? "#FFF4F4" :"black"}`,  border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}
@@ -40,7 +44,23 @@ const Projects = ({bgcolor,darkmode}) => {
               />
               <div>
 
-                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>Portfolio Website (Reactjs,Tailwind,Javascript)</h1>
+                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>E-commerce Website (Reactjs,Node.js)</h1>
+              </div>
+            </div>
+            </a>
+
+            <a href="https://lighthearted-zuccutto-7672ad.netlify.app/" target="_blank">
+            <div
+              className="ui-ux-design w-80 flex flex-col gap-3 p-5 rounded-lg"
+              style={{background:`${bgcolor==="white" ? "#FFF4F4" :"black"}`,  border:`${bgcolor==="white" ? "none" :"1px solid #A6A6A6"}`}}
+            >
+              <img
+                src={project3}
+                className="rounded-lg transition delay-150"
+              />
+              <div>
+
+                <h1 className={`text-md font-bold ${bgcolor==="white" ? "text-black" :"text-white"}`}>Portfolio Website (Reactjs,Tailwind CSS,Javascript)</h1>
               </div>
             </div>
             </a>
